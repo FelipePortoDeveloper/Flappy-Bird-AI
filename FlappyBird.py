8501b761a44dfee0b5a1d1c31f8b95911de75e60
Felipspp <felipeprates49@gmail.com>
2024-07-06 01:52:23 -0300

    Criação da classe do Cano

--- FlappyBird.py
+++ FlappyBird.py
@@ -42,6 +42,8 @@ class Passaro:
         self.contagem_imagem = 0
         self.imagem = self.IMGS[0]
 
+    # Funções
+
     def pular(self):
         self.velocidade = -10.5
         self.tempo = 0
@@ -77,9 +79,16 @@ class Passaro:
 
         self.contagem_imagem += 1
 
-        if self.contagem_imagem <= 15:
-            self.imagem = self.IMGS[floor(self.contagem_imagem / 3)]
-        else:
+        if self.contagem_imagem < self.TEMPO_ANIMACAO:
+            self.imagem = self.IMGS[0]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO * 2:
+            self.imagem = self.IMGS[1]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO * 3:
+            self.imagem = self.IMGS[2]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO * 4:
+            self.imagem = self.IMGS[1]
+        elif self.contagem_imagem >= self.TEMPO_ANIMACAO * 4 + 1:
+            self.imagem = self.IMGS[0]
             self.contagem_imagem = 0
 
         # Controlar batida de asa se caindo
@@ -97,3 +106,52 @@ class Passaro:
 
     def get_mask(self):
         pygame.mask.from_surface(self.imagem)
+
+
+class Cano:
+    # Constantes
+
+    DISTANCIA = 200
+    VELOCIDADE = 5
+
+    # Atributos
+
+    def __init__(self, x):
+        self.x = x
+        self.altura = 0
+        self.pos_topo = 0
+        self.pos_base = 0
+        self.CANO_TOPO = pygame.transform.flip(IMAGEM_CANO, False, True)
+        self.CANO_BASE = IMAGEM_CANO
+        self.passou = False
+        self.definir_altura()
+
+    # Funções
+
+    def definir_altura(self):
+        self.altura = random.randrange(50,450)
+        self.pos_topo = self.altura - self.CANO_TOPO.get_height()
+        self.pos_base = self.altura + self.DISTANCIA
+
+    def mover(self):
+        self.x -= self.VELOCIDADE
+
+    def desenhar(self, tela):
+        tela.blit(self.CANO_TOPO, (self.x, self.pos_topo))
+        tela.blit(self.CANO_BASE, (self.x, self.pos_base))
+
+    def colidir(self, passaro):
+        passaro_mask = passaro.get_mask()
+        topo_mask = pygame.mask.from_surface(self.CANO_TOPO)
+        base_mask = pygame.mask.from_surface(self.CANO_BASE)
+
+        distancia_topo = (self.x - passaro.x, self.pos_topo - round(passaro.y))
+        distancia_base =(self.x - passaro.x, self.pos_base - round(passaro.y))
+
+        topo_ponto = passaro_mask.overlap(topo_mask, distancia_topo)
+        base_ponto = passaro_mask.overlap(base_mask, distancia_base)
+
+        if topo_ponto or base_ponto:
+            return True
+        else:
+            return False
